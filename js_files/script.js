bad8274a90c6a5fbfa5c0a08372ca06b9e05767d
nubcynic <201962971+nubcynic@users.noreply.github.com>
2025-04-10 17:28:14 +0545

    feat: create function that gives random number

--- js_files/script.js
+++ js_files/script.js
@@ -1,1 +1,14 @@
-console.log("Hey Cynic!")
+console.log("Hey Cynic!"); // To check whether js file is linked to html
+
+//-------------- TOP Foundations - Rock Paper Scissor ----------------
+// ~~~~~~~ Step #1 Get computer choice
+
+// Create function "getComputerChoice"
+// Write code so that above function randomly returns "rock, paper or scissor"
+
+function getComputerChoice() {
+    let randomValue = Math.random();
+    console.log(randomValue);
+}
+
+getComputerChoice();
