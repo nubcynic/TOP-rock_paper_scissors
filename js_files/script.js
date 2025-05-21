c71580a6758f60b31ad1eb9f84df179799421670
nubcynic <201962971+nubcynic@users.noreply.github.com>
2025-05-21 13:39:05 +0545

    feat(playRound): update rule for any score > 5
    Added 1st rule to alert and end the game once any score reaches 5

--- js_files/script.js
+++ js_files/script.js
@@ -1,5 +1,4 @@
-// ~~~~~~~ Step #1 Get computer choice
-
+// Get computer choice
 function getComputerChoice() {
     let randomValue = Math.floor(Math.random() * 3) + 1;
     if (randomValue === 1) {
@@ -11,7 +10,6 @@ function getComputerChoice() {
     }
 };
 
-// ~~~~~~~~ Step #2 Get human choice
 
 // Reference to icons section which has images and buttons
 const iconSection = document.querySelector(".icons-section");
@@ -36,22 +34,34 @@ let scoreRecordComputer = 0;
 
 
 // btnList is a NodeList => [btn1, btn2, btn3]; so add event listener for each item from the list
+// e.target.textContent captures user choice
 btnList.forEach(item => {
     item.addEventListener("click", (e) => {
-        playRound(e.target.textContent, getComputerChoice());
+        playRound(e.target.textContent, getComputerChoice());  
     });
 });
 
+
+// Everytime button is clicked, run this function 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
+    // when any score reaches 5, alert
+    if (scoreRecordComputer === 5 || scoreRecordHuman === 5) {
+        if (scoreRecordComputer === 5) {
+            alert("Game Ended. Better Luck Next Time!");
+        }else {
+            alert("Game Ended. You are the WINNER!");
+        };
+    } else if (humanChoice === computerChoice) {
+        computerScore.textContent = scoreRecordComputer;
+        humanScore.textContent = scoreRecordHuman;
         resultListItem.textContent = `${humanChoice.toUpperCase()} v/s ${computerChoice.toUpperCase()}! It is a Draw`;
         resultList.appendChild(resultListItem);
     } else if ((humanChoice === "rock" && computerChoice === "paper")
             || (humanChoice === "paper" && computerChoice === "scissor")
             || (humanChoice === "scissor" && computerChoice === "rock")
         ) {
-            scoreRecordComputer++;
-            computerScore.textContent = scoreRecordComputer;
+            scoreRecordComputer++;          // add to the score
+            computerScore.textContent = scoreRecordComputer;    // put the score in the page
             resultListItem.textContent = `${humanChoice.toUpperCase()} v/s ${computerChoice.toUpperCase()}! You Loose`;
             resultList.appendChild(resultListItem);
     } else {
@@ -65,64 +75,3 @@ function playRound(humanChoice, computerChoice) {
 
 
 
-
-
-
-
-
-// // ~~~~~~~~ Step #3 Declare scores
-
-// // Create two variables "humanScore" and "computerScore" in the global scope
-// // Assign it a value of 0 initially
-// let humanScore = 0;
-// let computerScore = 0;
-// let round = 0;
-
-// // ~~~~~~~~ Step #4 Play single round
-
-// // Create a function "playRound"
-// // Use two parameters "humanChoice" and "computerChoice" which takes both choices as arguments
-// // Make "humanChoice" parameter case insensitive
-// // Write the code so that the winner is printed based off both choices and game rules
-// // Increment the score based on whoever wins the round
-
-// function playRound(userChoice, computerChoice) {
-//     if (userChoice === computerChoice) {
-//         console.log(`Computer's choice: ${computerChoice}\nYour Choice: ${userChoice}\nIt's a Draw`);
-//     } else if (userChoice === "rock" && computerChoice === "scissor") {
-//         humanScore++;
-//         console.log(`Computer's choice: ${computerChoice}\nYour Choice: ${userChoice}\nYou Win! ${userChoice} beats ${computerChoice}`);
-//     } else if (userChoice === "paper" && computerChoice === "rock") {
-//         humanScore++;
-//         console.log(`Computer's choice: ${computerChoice}\nYour Choice: ${userChoice}\nYou Win! ${userChoice} beats ${computerChoice}`);
-//     } else if (userChoice === "scissor" && computerChoice === "paper") {
-//         humanScore++;
-//         console.log(`Computer's choice: ${computerChoice}\nYour Choice: ${userChoice}\nYou Win! ${userChoice} beats ${computerChoice}`);
-//     } else {
-//         computerScore++;
-//         console.log(`Computer's choice: ${computerChoice}\nYour Choice: ${userChoice}\nYou Lose! ${computerChoice} beats ${userChoice}`);
-//     }
-// }
-
-// // ~~~~~~~~~~~~ Step #5 Play the entire game
-
-// // Create function "playGame"
-// // Move "playRound" function and score variables inside
-// // Play the rounds 5 times
-
-// function playGame () {
-//     while (round < 5) {
-//         round++;
-//         playRound(getHumanChoice(), getComputerChoice());
-//         console.log(`round no: ${round}\nYour score: ${humanScore}\nComputer Score : ${computerScore}`);
-//     };
-//     if (humanScore > computerScore) {
-//         console.log(`WINNER!\nYou won ${humanScore} rounds out of ${round} rounds`);
-//     } else if (computerScore > humanScore) {
-//         console.log(`BETTER LUCK NEXT TIME!\nYou only won ${humanScore} rounds out of ${round} rounds`);
-//     } else {
-//         console.log(`ITS A TIE!\nYour score: ${humanScore}\nComputer's Score: ${computerScore}`);
-//     }
-// } 
-    
-// playGame();
